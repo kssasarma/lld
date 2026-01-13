26e28c09f81477f29cf75c9dd9935214a753217a
ask <kssasarma@gmail.com>
2026-01-13 16:58:05 +0530

    Prototype pattern

---
+++ src/main/java/com/kssasarma/behavioural/prototype/Client.java
@@ -0,0 +1,18 @@
+package com.kssasarma.behavioural.prototype;
+
+public class Client {
+    public static void main(String[] args) {
+        Vehicle car1 = new Car(4, 500000, "Red");
+        Vehicle car2 = car1.clone();
+
+        System.out.println(car1 == car2); // false
+        System.out.println("Car1 is clone of Car2: " + car1.isClone(car2));
+
+        Vehicle bicycle1 = new Bicycle(2, 20000, true);
+        Vehicle bicycle2 = bicycle1.clone();
+        System.out.println(bicycle1 == bicycle2); // false
+        System.out.println("Bicycle1 is clone of Bicycle2: " + bicycle1.isClone(bicycle2));
+
+        System.out.println("Car1 is clone of Bicycle1: " + car1.isClone(bicycle1));
+    }
+}
